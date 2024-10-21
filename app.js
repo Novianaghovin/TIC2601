2832f7ead0b7f43cd7bfe2dea28ef54173573efd
Novianaghovin <e1062194@u.nus.edu>
2024-10-22 00:05:53 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// Import the sqlite3 library with verbose mode for additional debugging information
+const sqlite3 = require('sqlite3').verbose();
+
+// Connect to the SQLite database
+// './ConnectToDB.db' refers to the relative path of the database file
+// sqlite3.OPEN_READWRITE mode allows both reading and writing to the database
+const db = new sqlite3.Database('./ConnectToDB.db', sqlite3.OPEN_READWRITE, (err) => {
+    
+    // If there is an error while connecting, log the error message and stop the execution
+    if (err) return console.error(err.message);
+    
+    // If no error occurs, log a message indicating the successful connection to the database
+    console.log('Connected to the database.');
+});
